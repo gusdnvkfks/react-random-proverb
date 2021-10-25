de93d425a647918ae6c58365eae8fb1e8eb9e5a2
gusdnvkfks <kbs2415@naver.com>
2021-10-25 14:41:33 +0900

    random-proverb

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const proverbs = ["좌절감으로 배움을 늦추지 마라.", "삶이 있는 한 희망은 있다." , "언제나 현재에 집중할수 있다면 행복할것이다.", "좋은 성과를 얻으려면 한 걸음 한 걸음이 힘차고 충실하지 않으면 안 된다.", "행복은 습관이다,그것을 몸에 지니라."];
+
+  const getRandomIndex = (length) => {
+    return parseInt(Math.random() * length);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code> 파일을 고치고 저장하면 새로운 명언을 볼 수 있습니다.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {proverbs[getRandomIndex(proverbs.length)]}
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
+// import를 이용해서 nodejs에 설치된 패키지를 불러오거나 src디렉토리에 있는 js 파일도 불러올 수 있다.
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
